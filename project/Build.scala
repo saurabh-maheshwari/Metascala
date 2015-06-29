b2f0aefd0d402597d96df7db784b9b2ea6cd36d7
Saurabh Maheshwari <smaheshwari@sambasafety.com>
2015-06-29 16:03:40 -0600

    Upgrading jars

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object Build extends sbt.Build{
       Defaults.defaultSettings ++ assemblySettings ++ Seq(
       organization  := "com.example",
       version       := "0.1",
-      scalaVersion  := "2.10.2",
+      scalaVersion  := "2.10.5",
 
       resolvers ++= Seq(
         "typesafe repo"      at "http://repo.typesafe.com/typesafe/releases/",
@@ -22,9 +22,9 @@ object Build extends sbt.Build{
       ),
       libraryDependencies ++= Seq(
         "org.ow2.asm" % "asm-debug-all" % "4.1",
-        "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
-        "org.mozilla" % "rhino" % "1.7R4",
-        "com.nativelibs4java" % "scalaxy-loops_2.10" % "0.3-SNAPSHOT" % "provided"
+        "org.scalatest" % "scalatest_2.10" % "2.2.5" % "test",
+        "org.mozilla" % "rhino" % "1.7.7",
+        "com.nativelibs4java" % "scalaxy-loops_2.10" % "0.3.4" % "provided"
       )
     )
   )
